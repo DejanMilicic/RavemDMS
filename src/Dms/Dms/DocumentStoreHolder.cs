22756653ffa2661947554098c2186e1771790009
Dejan Milicic <dejan.milicic@gmail.com>
2021-11-24 00:47:58 +0100

    wip

---
+++ src/Dms/Dms/DocumentStoreHolder.cs
@@ -0,0 +1,25 @@
+﻿namespace Dms;
+
+using Raven.Client.Documents;
+using Raven.Client.Documents.Indexes;
+
+public static class DocumentStoreHolder
+    {
+        private static readonly Lazy<IDocumentStore> LazyStore =
+            new Lazy<IDocumentStore>(() =>
+            {
+                IDocumentStore store = new DocumentStore
+                {
+                    Urls = new[] { "http://127.0.0.1" },
+                    Database = "dms"
+                };
+
+                store.Initialize();
+
+                IndexCreation.CreateIndexes(typeof(Program).Assembly, store);
+
+                return store;
+            });
+
+        public static IDocumentStore Store => LazyStore.Value;
+    }
