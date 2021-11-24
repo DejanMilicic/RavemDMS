7e6aa6219d56a1258b0f9adad4b34e6e968dc8c2
Dejan Milicic <dejan.milicic@gmail.com>
2021-11-24 09:26:17 +0100

    indexing word documents

--- src/Dms/Dms/ExtractText.cs
+++ src/Dms/Dms/ExtractText.cs
@@ -1,16 +1,46 @@
 ﻿namespace Dms;
 
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
 using System;
 using System.Collections.Generic;
 
 public static class ExtractText
 {
-    public static string[] FromAttachment(string fileName)
+    public static IEnumerable<string> FromAttachment(string fileName, Stream stream)
     {
-        return new string[]
+        if (fileName.ToLower().EndsWith(".docx"))
         {
-            fileName
-        };
+            return GetWordText(stream);
+        }
+        else
+        {
+            return new string[]
+            {
+                fileName
+            };
+        }
+    }
+
+    // nuget: DocumentsFormat.OpenXml
+    public static IEnumerable<string> GetWordText(Stream stream)
+    {
+        using var doc = WordprocessingDocument.Open(stream, false);
+        foreach (var element in doc.MainDocumentPart.Document.Body)
+        {
+            if (element is Paragraph p)
+            {
+                yield return p.InnerText;
+            }
+        }
+
+        var comments = doc.MainDocumentPart?.WordprocessingCommentsPart?.Comments;
+        if (comments == null)
+            yield break;
+        foreach (var element in comments)
+        {
+            yield return element.InnerText;
+        }
     }
 }
 
--- src/Dms/Dms/Search.cs
+++ src/Dms/Dms/Search.cs
@@ -15,31 +15,69 @@ public class Search : AbstractIndexCreationTask<Doc>
     public Search()
     {
         Map = docs => from doc in docs
-            let attachments = AttachmentsFor(doc)
-            from att in attachments
-            select new
-            {
-                Query = ExtractText.FromAttachment(att.Name)
-            };
+                let attachments = AttachmentsFor(doc)
+                from att in attachments
+                    let stream = LoadAttachment(doc, att.Name).GetContentAsStream()
+                    select new
+                    {
+                        Query = ExtractText.FromAttachment(att.Name, stream)
+                    };
 
         Index("Query", FieldIndexing.Search);
+
+        AdditionalAssemblies = new HashSet<AdditionalAssembly>()
+        {
+            AdditionalAssembly.FromNuGet("DocumentFormat.OpenXml", "2.14.0")
+        };
 
         AdditionalSources = new Dictionary<string, string>
         {
             ["ExtractText.cs"] = @"
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
 using System;
 using System.Collections.Generic;
 
 public static class ExtractText
 {
-    public static string[] FromAttachment(string fileName)
+    public static IEnumerable<string> FromAttachment(string fileName, Stream stream)
     {
-        return new string[]
+        if (fileName.ToLower().EndsWith("".docx""))
+            {
+            return GetWordText(stream);
+        }
+        else
         {
-            fileName
-        };
+            return new string[]
+            {
+                fileName
+            };
+        }
+        }
+
+        // nuget: DocumentsFormat.OpenXml
+        public static IEnumerable<string> GetWordText(Stream stream)
+        {
+            using var doc = WordprocessingDocument.Open(stream, false);
+            foreach (var element in doc.MainDocumentPart.Document.Body)
+            {
+                if (element is Paragraph p)
+                {
+                    yield return p.InnerText;
+                }
+            }
+
+            var comments = doc.MainDocumentPart?.WordprocessingCommentsPart?.Comments;
+            if (comments == null)
+                yield break;
+            foreach (var element in comments)
+            {
+                yield return element.InnerText;
+            }
+        }
     }
-}
+
+
 
 
 "
